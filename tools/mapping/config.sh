27b116da32583fef1c3a12806f8c782e30eec4da
Christopher Geiger <christophergeiger3@gmail.com>
2019-12-24 14:29:24 -0700

    Add Variables.env file, which is now generated through config.sh and read by Makefile

---
+++ tools/mapping/config.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+( set -o posix ; set ) >/tmp/variables.before
+
+# Example configuration
+
+cesm_root=/glade/u/home/chrgeiger/cesm1_2_0
+gen_mapping_files=${cesm_root}/tools/mapping/gen_mapping_files
+
+nx=320                       # number of i ocn gridpoints (longitudes)
+ny=394                       # number of j ocn gridpoints (latitudes)
+
+scripdir=/gpfs/fs1/p/cgd/ccr/people/ranfeng/plio_bnd/PlioMIP_enh/cpl_mapping/scrip1.4/
+
+
+# -----------------------------------------------------------------
+# Input files -----------------------------------------------------
+# -----------------------------------------------------------------
+
+popgridfile=${gen_mapping_files}/horiz_grid_plioenh_070820.ieeer8
+kmtgridfile=${gen_mapping_files}/topography_plio_enh_gx1v6_190619.CESM2_modMed.ieeei4
+
+# -----------------------------------------------------------------
+# Output labels -----------------------------------------------------
+# -----------------------------------------------------------------
+ocngridname='plio_enh'
+
+# -----------------------------------------------------------------
+# Output filenames ------------------------------------------------
+# -----------------------------------------------------------------
+DATE=$(date +%y%m%d)
+ocnres='gx1v6plioenh'
+ocngridfile=${ocnres}_${DATE}.nc
+
+#TODO
+#HERESTRING=$(cat << EOF
+#          &input_nml
+#           gridtype     = 'rtm'
+#           file_roff    = '/glade/p/cesm/cseg/inputdata/lnd/clm2/rtmdata/rdirc.05.061026'
+#           file_ocn     = '../gx1v6plioenh_${DATE}.nc'
+#           file_nn      = 'map_r05_to_gx1v6plioenh_nn_${DATE}.nc '
+#           file_smooth  = 'map_gx1v6_to_gx1v6plioenh_sm_e1000r300_${DATE}.nc '
+#           file_new     = 'map_r05_to_gx1v6plioenh_nnsm_e1000r300_${DATE}.nc'
+#           title        = 'runoff map: r05 -> gx1v6, nearest neighbor and smoothed '
+#           eFold        = 1000000.0
+#           rMax         =  300000.0
+#           step1 = .true.
+#           step2 = .true.
+#           step3 = .true.
+#          /
+#        EOF
+#)
+
+( set -o posix ; set ) >/tmp/variables.after
+
+>Variables.env
+for var in $(diff -e /tmp/variables.before /tmp/variables.after | sort)
+do
+	if [[ $var == *"="* ]]; then
+		printf "$var\n" >> Variables.env
+	fi
+done
