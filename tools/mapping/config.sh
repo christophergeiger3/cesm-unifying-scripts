bad17bb188a746c26cdf0bc60636262da8571f1a
Christopher Geiger <christophergeiger3@gmail.com>
2020-03-03 16:25:18 -0700

    Update Makefile, Defaults, config with new variable names

--- tools/mapping/config.sh
+++ tools/mapping/config.sh
@@ -43,6 +43,7 @@ tag=plioenh
 ocn_res=gx1v6
 atm_res=fv0.9_1.25
 rof_res=05
+natm=fv1.9_2.5
 fatm=/glade/p/cesm/cseg/mapping/grids/${natm}_070727.nc
 focn=${gen_mapping_files}/${ocn_res}${tag}_${DATE}.nc
 nocn=${ocn_res}${tag}
@@ -52,9 +53,9 @@ file_smooth=map_r${rof_res}_to_${ocn_res}${tag}_sm_e1000r300_${DATE}.nc
 file_new=map_r${rof_res}_to_${ocn_res}${tag}_nnsm_e1000r300_${DATE}.nc
 fsrc=/glade/p/cesm/cseg/mapping/grids/r${rof_res}_nomask_070925.nc
 nsrc=r${rof_res}_nomask
+
+
 # Ask ran about this: #-l fv1.9_2.5  \      # feng: this should be the same as fv09_1.25, inherited from Variable.env
-prefix2=fv1.9_2.5 # rfeng: This should be consistent with natm
-plio=plioenh # delete this
 
 #TODO
 #HERESTRING=$(cat << EOF
